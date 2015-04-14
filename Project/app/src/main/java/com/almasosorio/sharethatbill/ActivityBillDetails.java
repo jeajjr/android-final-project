cdf81e2f8584d3920cbb4aa30e7c1be1f63f99fa
jeajjr <jeajjr@gmail.com>
2015-04-14 00:57:07 -0300

    Started creating BillDetails Activity/Fragment.

---
+++ Project/app/src/main/java/com/almasosorio/sharethatbill/ActivityBillDetails.java
@@ -0,0 +1,20 @@
+package com.almasosorio.sharethatbill;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class ActivityBillDetails extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bill_details);
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.container, new FragmentBillDetails())
+                .commit();
+    }
+}
