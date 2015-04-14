e7d1ca6bcf032269b7a85f23c3c07b33eefce4ef
jeajjr <jeajjr@gmail.com>
2015-04-14 13:27:59 -0300

    Added delete button to FragmentBillDetails.

--- Project/app/src/main/java/com/almasosorio/sharethatbill/FragmentBillDetails.java
+++ Project/app/src/main/java/com/almasosorio/sharethatbill/FragmentBillDetails.java
@@ -3,13 +3,15 @@ package com.almasosorio.sharethatbill;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class FragmentBillDetails extends Fragment {
-
+    private static final String TAG = "FragmentBillDetails";
 
     public FragmentBillDetails() {
         // Required empty public constructor
@@ -20,7 +22,19 @@ public class FragmentBillDetails extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bill_details, container, false);
+        View v = inflater.inflate(R.layout.fragment_bill_details, container, false);
+
+        View deleteButton = v.findViewById(R.id.layout_delete_button);
+        TextView buttonText = (TextView) v.findViewById(R.id.delete_button_text);
+        buttonText.setText(buttonText.getText().toString().toUpperCase());
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "clicked on deleteButton");
+            }
+        });
+
+        return v;
     }
 
 
