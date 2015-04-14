ef181a0b04f01ab0b4648f378406bddb1e12a297
jeajjr <jeajjr@gmail.com>
2015-04-13 23:21:04 -0300

    Finished login screen.

--- Project/app/src/main/java/com/almasosorio/sharethatbill/FragmentLogin.java
+++ Project/app/src/main/java/com/almasosorio/sharethatbill/FragmentLogin.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -22,8 +23,15 @@ public class FragmentLogin extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View v = inflater.inflate(R.layout.fragment_login, container, false);
+
+        Button loginButton = (Button) v.findViewById(R.id.button_login);
+        loginButton.setText(loginButton.getText().toString().toUpperCase());
+
+        Button createAccountButton = (Button) v.findViewById(R.id.button_create_account);
+        createAccountButton.setText(createAccountButton.getText().toString().toUpperCase());
+
+        return v;
     }
 
 
